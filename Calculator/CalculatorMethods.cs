562ceec9d77a4194f48715c1c8ad716c6379f111
adiboy6 <aditya.reddy.97@gmail.com>
2020-01-25 13:52:35 +0530

    initial commit

---
+++ Calculator/CalculatorMethods.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    public class CalculatorMethods
+    {
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int subtract(int a, int b)
+        {
+            return a - b;
+        }
+        public int multiply(int a, int b)
+        {
+            return a * b;
+        }
+        public double divide(double a, double b)
+        {
+            if (b == 0.0f)
+            {
+                throw new DivideByZeroException();
+            }
+            return a / b;
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            CalculatorMethods calculatorMethods = new CalculatorMethods();
+
+            int addition_ans = calculatorMethods.add(Int32.MinValue, Int32.MinValue);
+
+            //Sample Run
+            Console.WriteLine(addition_ans);
+
+
+            //int subtraction_ans = CalculatorMethods.subtract(Int32.MaxValue, Int32.MaxValue);
+            
+            //Sample Run
+            //Console.WriteLine(subtraction_ans);
+
+
+            double division_ans = calculatorMethods.divide(22, 7);
+            
+            //Sample Run
+            Console.WriteLine(division_ans);
+        }
+    }
+}
